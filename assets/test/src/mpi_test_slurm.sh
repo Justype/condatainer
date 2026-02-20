27107ee66d9ea6e463b6f0a822e388c694cdff4b
Justype <justype.net@outlook.com>
2026-02-20 13:21:57 -0500

    feat: enhance job testing output with additional resource information

--- assets/test/src/mpi_test_slurm.sh
+++ assets/test/src/mpi_test_slurm.sh
@@ -9,5 +9,17 @@
 
 #DEP:mpi.img
 
+echo ======In Job Testing========
+echo In Job Testing
+echo NNODES $NNODES
+echo NTASKS $NTASKS
+echo NTASKS_PER_NODE $NTASKS_PER_NODE
+echo NCPUS $NCPUS
+echo NCPUS_PER_TASK $NCPUS_PER_TASK
+echo MEM $MEM
+echo MEM MB $MEM_MB
+echo MEM GB $MEM_GB
+echo ============================
+
 # Run under test folder not the src folder.
 python src/mpi_file_handler.py
--- assets/test/src/run_chain_lsf.sh
+++ assets/test/src/run_chain_lsf.sh
@@ -9,10 +9,15 @@
 echo IN_CONDATAINER "$IN_CONDATAINER"
 echo SALMON_INDEX_DIR $SALMON_INDEX_DIR
 ls -l $SALMON_INDEX_DIR
+echo ======In Job Testing========
+echo In Job Testing
 echo NNODES $NNODES
 echo NTASKS $NTASKS
+echo NTASKS_PER_NODE $NTASKS_PER_NODE
 echo NCPUS $NCPUS
+echo NCPUS_PER_TASK $NCPUS_PER_TASK
 echo MEM $MEM
 echo MEM MB $MEM_MB
 echo MEM GB $MEM_GB
+echo ============================
 salmon --version
